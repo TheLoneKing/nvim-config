4f111fa8c43b9761707f4255a28565fcb99e1979
TheLoneKing <dkumar.kovai@gmail.com>
2022-09-28 23:35:51 -0400

    Update lualine config

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -6,10 +6,56 @@ else
 	return
 end
 
+local hide_in_width = function()
+  return vim.fn.winwidth(0) > 80
+end
+
+local diagnostics = {
+  'diagnostics',
+  sources = { 'nvim_diagnostic' },
+  sections = { 'error', 'warn' },
+  symbols = { error = ' ', warn = ' ' },
+  colored = false,
+  always_visible = true,
+}
+
+local diff = {
+  'diff',
+  colored = false,
+  symbols = { added = ' ', modified = ' ', removed = ' ' }, -- changes diff symbols
+  cond = hide_in_width,
+}
+
+local spaces = function()
+  return 'spaces: ' .. vim.api.nvim_buf_get_option(0, 'shiftwidth')
+end
+
+local filetype = {
+  'filetype',
+  icons_enabled = true,
+}
+
+local location = {
+  'location',
+  padding = 0,
+}
+
 lualine.setup({
   options = {
-    theme = 'papercolor_light',
+    theme = 'auto',
     section_separators = '',
-    component_separators = ''
+    component_separators = '',
+    disabled_filetypes = { 'alpha', 'dashboard' },
+    globalstatus = true,
+    icons_enabled = true,
+    always_divide_middle = true
+  },
+  sections = {
+    lualine_a = { 'mode' },
+    lualine_b = {'branch'},
+    lualine_c = { diagnostics, 'filename' },
+    lualine_x = { diff, spaces, 'encoding', filetype },
+    lualine_y = { 'progress' },
+    lualine_z = { location }
   }
 })
